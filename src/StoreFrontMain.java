d95bcc4b6c4d29d35502d437ceafff4daa060e12
TRACE E DAVIS <160071752+Trace0727@users.noreply.github.com>
2025-05-13 11:41:36 -0400

    Add files via upload

---
+++ src/StoreFrontMain.java
@@ -0,0 +1,9 @@
+//nothing else needs to be added to this class.
+public class StoreFrontMain {
+
+	public static void main(String[] args) {
+		
+		StoreFrontGUI frame = new StoreFrontGUI();
+		frame.setVisible(true);
+	}
+}
