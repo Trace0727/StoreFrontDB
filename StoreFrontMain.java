1a013fa29f321e7acef49542e9279fb84ff0be65
TRACE E DAVIS <160071752+Trace0727@users.noreply.github.com>
2025-05-13 11:41:52 -0400

    Delete StoreFrontMain.java

--- StoreFrontMain.java
+++
@@ -1,9 +0,0 @@
-//nothing else needs to be added to this class.
-public class StoreFrontMain {
-
-	public static void main(String[] args) {
-		
-		StoreFrontGUI frame = new StoreFrontGUI();
-		frame.setVisible(true);
-	}
-}
